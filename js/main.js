a125c4f46649fac31adbbaeebebbf9c85c684e9b
SOHEE <118507151+devheee@users.noreply.github.com>
2022-12-05 17:50:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,28 +5,30 @@ $(function () {
     })
 
     $('.mainSlide').slick({
+        arrows: false,
         dots: true,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2500,
         pauseOnHover: false,
         pauseOnFocus: false,
 
         customPaging: function (slider, i) {
             var thumb = $(slider.$slides[i]).data();
             if (i == '0') {
-                i = "기업시민";
+                i = "홈서비스 타임딜";
             } else if (i == '1') {
-                i = "스마트컨스트럭션";
+                i = "위클리 특가";
             } else if (i == '2') {
-                i = "탄소중립경영";
+                i = "침수차 보상 프로그램";
             } else if (i == '3') {
-                i = "더샵";
-            } else if (i == '4') {
-                i = "도시정비";
-            } else if (i == '5') {
-                i = "기업문화";
+                i = "렌트";
             } return '<a class="dot">' + i + '</a>';
-        },
+        }
+
+    });
+
+    $('.mainSlide').on('afterChange', function (e, s, c) {
+        $('.mainVisual .main_num').text(c + 1 + "/" + s.slideCount);
     });
 
     $('.subSlide').slick({
@@ -38,9 +40,7 @@ $(function () {
         pauseOnFocus: false,
     });
 
-    $('.subSlide').on('afterChange', function (e, s, c) {
-        $('.subBussiness .sub_num').text(c + 1 + "/" + s.slideCount);
-    });
+
 
     $('.popup button').on('click', function () {
         $(this).parent().hide();
